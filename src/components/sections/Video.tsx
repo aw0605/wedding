0324e502187730af0f247ba6b44733ff58a8949b
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-07-23 20:57:24 +0900

    Video 구현

--- src/components/sections/Video.tsx
+++ src/components/sections/Video.tsx
@@ -6,7 +6,14 @@ import styles from './Video.module.scss'
 const cx = classNames.bind(styles)
 
 function Video() {
-  return <Section>Video</Section>
+  return (
+    <Section className={cx('container')}>
+      <video autoPlay loop muted poster="/assets/poster.jpg">
+        <source src="/assets/main.webm" type="video/webm" />
+        <source src="/assets/main.mp4" type="video/mp4" />
+      </video>
+    </Section>
+  )
 }
 
 export default Video
